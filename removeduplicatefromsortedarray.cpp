cc609f55db6b9b98cc0af21a23a932c8ef04360d
shree29102006 <bhavyagupta2906@gmail.com>
2025-08-11 20:46:41 +0530

    brute approach

---
+++ removeduplicatefromsortedarray.cpp
@@ -0,0 +1,12 @@
+int removeDuplicates(int arr[], int n) {
+  set < int > set;
+  for (int i = 0; i < n; i++) {
+    set.insert(arr[i]);
+  }
+  int k = set.size();
+  int j = 0;
+  for (int x: set) {
+    arr[j++] = x;
+  }
+  return k;
+}
